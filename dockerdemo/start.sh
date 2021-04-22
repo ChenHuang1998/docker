49f52ac9e1122fa201a88000af6ca41b5ca90f04
ChenHuang1998 <hys19980630@163.com>
2021-04-22 16:17:20 +0800

    init9

--- dockerdemo/start.sh
+++ dockerdemo/start.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --noinput&&
 python manage.py makemigrations&&
 python manage.py migrate&&
-gunicorn my_blog.wsgi:application -c gunicorn.conf
+gunicorn dockerdemo.wsgi:application -c gunicorn.conf
